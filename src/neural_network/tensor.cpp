555cd5b7f4f054e39f2b7f324ad368cce82cf003
xyb <xyb@lotx.org>
2025-03-14 12:28:32 -0700

    Rename Tensor::Backward() to Tensor::Derivative()

--- src/neural_network/tensor.cpp
+++ src/neural_network/tensor.cpp
@@ -1,6 +1,7 @@
 /// @file
 module;
 
+#include <atomic>
 #include <cstddef>
 #include <functional>
 #include <initializer_list>
@@ -8,7 +9,6 @@ module;
 #include <span>
 #include <stdexcept>
 #include <stdfloat>
-#include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -20,6 +20,8 @@ export import :backend;
 
 namespace cpp_matrix::neural_network {
 
+static std::atomic_uint32_t s_tensor_id_seed {};
+
 /// @brief Tensor is a matrix supports auto grad.
 export template <Backend Matrix>
 class Tensor {
@@ -28,29 +30,15 @@ public:
 
     /// @brief Initialize a tensor with matrix data.
     Tensor(Matrix m)
-        : Tensor { "", std::move(m) }
-    {
-    }
-
-    /// @brief Initialize a tensor with label and matrix data.
-    Tensor(std::string label, Matrix m)
-        : m_label { std::move(label) }
-        , m_matrix { std::move(m) }
-        , m_grads { { m_label, { m_matrix.Row(), m_matrix.Column(), (ElementType)1 } } }
+        : m_matrix { std::move(m) }
+        , m_grads { { m_id, { m_matrix.Row(), m_matrix.Column(), (ElementType)1 } } }
     {
     }
 
     /// @brief Initialize a tensor with init data.
     Tensor(std::initializer_list<std::initializer_list<ElementType>> initData)
-        : Tensor { "", std::move(initData) }
-    {
-    }
-
-    /// @brief Initialize a tensor with label and init data.
-    Tensor(std::string label, std::initializer_list<std::initializer_list<ElementType>> initData)
-        : m_label { std::move(label) }
-        , m_matrix { std::move(initData) }
-        , m_grads { { m_label, { m_matrix.Row(), m_matrix.Column(), (ElementType)1 } } }
+        : m_matrix { std::move(initData) }
+        , m_grads { { m_id, { m_matrix.Row(), m_matrix.Column(), (ElementType)1 } } }
     {
     }
 
@@ -165,7 +153,7 @@ public:
     Tensor Exp() const
     {
         auto r = m_matrix.Exp();
-        auto res = Tensor { m_label, r };
+        auto res = Tensor { r };
         res.m_grads = m_grads;
         for (auto& g : res.m_grads) {
             g.second = r.ElementProduct(g.second);
@@ -202,10 +190,67 @@ public:
         return res;
     }
 
-    /// @brief Compute gradients.
-    Tensor Backward(const std::string& label) const
+    /** @brief Get derivative of tensor at \c x.
+    
+        This function will return derivative of tensor at \c x.
+
+        For example:
+        \code{.cpp}
+        auto x = Tensor {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+        };
+
+        auto y = Tensor {
+            { 0.1, 0.2, 0.3 },
+            { 0.4, 0.5, 0.6 },
+        };
+
+        auto f = 3*x + 4*y;
+        auto dx = f.Derivative(x);
+        auto dy = f.Derivative(y);
+        \endcode
+
+        Because:
+
+        \f[
+        \begin{aligned}
+                    f &= 3 \cdot x + 4 \cdot y \\\\
+        {d \over dx}f &= {d \over dx}(3 \cdot x) + {d \over dx}(4 \cdot y) \\
+                      &= 3 \cdot {d \over dx}x + 4 \cdot {d \over dx}y \\
+                      &= 3 \\\\
+        {d \over dy}f &= {d \over dy}(3 \cdot x) + {d \over dy}(4 \cdot y) \\
+                      &= 3 \cdot {d \over dy}x + 4 \cdot {d \over dy}y \\
+                      &= 4
+        \end{aligned}
+        \f]
+
+        So after executing, \c f, \c dx and \c dy should be:
+        \f[
+        \begin{aligned}
+        f &= 
+        \begin{bmatrix}
+             3.4 &  6.8 & 10.2 \\
+            13.6 & 17.0 & 20.4
+        \end{bmatrix}
+        \\\\
+        dx &=
+        \begin{bmatrix}
+            3 & 3 & 3 \\
+            3 & 3 & 3
+        \end{bmatrix}
+        \\\\
+        dy &=
+        \begin{bmatrix}
+            4 & 4 & 4 \\
+            4 & 4 & 4
+        \end{bmatrix}
+        \end{aligned}
+        \f]
+    */
+    Tensor Derivative(const Tensor& x) const
     {
-        auto it = m_grads.find(label);
+        auto it = m_grads.find(x.m_id);
         if (it == m_grads.end()) {
             return {};
         } else {
@@ -213,22 +258,20 @@ public:
         }
     }
 
-    /// @brief Compute gradients.
-    Tensor Backward() const
+    /// @brief Returns the sum of all elements in the tensor.
+    /// @return Should be a \a 1x1 Tensor.
+    Tensor SumX() const
     {
-        if (m_grads.size() > 1) {
-            throw std::runtime_error { "Label must be specified for multiple inputs." };
-        } else if (m_grads.empty()) {
-            return {};
-        } else {
-            return m_grads.begin()->second;
-        }
+        auto res = Tensor { m_matrix.Sum() };
+        res.m_grads = m_grads;
+        return res;
     }
 
+
 private:
-    std::string m_label {};
+    const std::atomic_uint32_t m_id { s_tensor_id_seed++ };
     Matrix m_matrix {};
-    std::unordered_map<std::string, Matrix> m_grads {};
+    std::unordered_map<uint32_t, Matrix> m_grads {};
     mutable std::optional<std::vector<ElementType>> m_data {};
 };
 
--- test/neural_network_test/tensor_test.cpp
+++ test/neural_network_test/tensor_test.cpp
@@ -1,20 +1,56 @@
-NEURAL_NETWORK_TEST(Tensor, AutoGrad)
+NEURAL_NETWORK_TEST(Tensor, AutoGrad1)
 {
-    auto x = Tensor { "x",
-        {
-            { 1.0_mf, 1.1_mf, 1.2_mf },
-            { 2.0_mf, 2.2_mf, 2.3_mf },
-        } };
+    auto x = Tensor { {
+        { 1.0_mf, 2.0_mf, 3.0_mf },
+        { 4.0_mf, 5.0_mf, 6.0_mf },
+    } };
 
-    auto y = Tensor { "y",
-        {
-            { 1.0_mf, 1.1_mf, 1.2_mf },
-            { 2.0_mf, 2.2_mf, 2.3_mf },
-        } };
+    auto y = Tensor { {
+        { 0.1_mf, 0.2_mf, 0.3_mf },
+        { 0.4_mf, 0.5_mf, 0.6_mf },
+    } };
+
+    auto f = 3.0_mf * x + 4.0_mf * y;
+    auto dx = f.Derivative(x);
+    auto dy = f.Derivative(y);
+
+    ASSERT_FLOAT_EQ((f[0, 0]), 3.4_mf);
+    ASSERT_FLOAT_EQ((f[0, 1]), 6.8_mf);
+    ASSERT_FLOAT_EQ((f[0, 2]), 10.2_mf);
+    ASSERT_FLOAT_EQ((f[1, 0]), 13.6_mf);
+    ASSERT_FLOAT_EQ((f[1, 1]), 17.0_mf);
+    ASSERT_FLOAT_EQ((f[1, 2]), 20.4_mf);
+
+    ASSERT_FLOAT_EQ((dx[0, 0]), 3.0_mf);
+    ASSERT_FLOAT_EQ((dx[0, 1]), 3.0_mf);
+    ASSERT_FLOAT_EQ((dx[0, 2]), 3.0_mf);
+    ASSERT_FLOAT_EQ((dx[1, 0]), 3.0_mf);
+    ASSERT_FLOAT_EQ((dx[1, 1]), 3.0_mf);
+    ASSERT_FLOAT_EQ((dx[1, 2]), 3.0_mf);
+
+    ASSERT_FLOAT_EQ((dy[0, 0]), 4.0_mf);
+    ASSERT_FLOAT_EQ((dy[0, 1]), 4.0_mf);
+    ASSERT_FLOAT_EQ((dy[0, 2]), 4.0_mf);
+    ASSERT_FLOAT_EQ((dy[1, 0]), 4.0_mf);
+    ASSERT_FLOAT_EQ((dy[1, 1]), 4.0_mf);
+    ASSERT_FLOAT_EQ((dy[1, 2]), 4.0_mf);
+}
+
+NEURAL_NETWORK_TEST(Tensor, AutoGrad2)
+{
+    auto x = Tensor { {
+        { 1.0_mf, 1.1_mf, 1.2_mf },
+        { 2.0_mf, 2.2_mf, 2.3_mf },
+    } };
+
+    auto y = Tensor { {
+        { 1.0_mf, 1.1_mf, 1.2_mf },
+        { 2.0_mf, 2.2_mf, 2.3_mf },
+    } };
 
     auto z = 3.0_mf * x.Pow(2) + 2.0_mf * y;
-    auto dx = z.Backward("x");
-    auto dy = z.Backward("y");
+    auto dx = z.Derivative(x);
+    auto dy = z.Derivative(y);
 
     auto expectDx
         = [](typename Matrix::ElementType v) { return 3.0_mf * 2.0_mf * (typename Matrix::ElementType)pow(v, 1.0_mf); };
